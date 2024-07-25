9b3cb7ef38fbf99c908be95f0b724c2866d8976e
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2024-07-25 04:52:46 -0300

    Add file_path parameter to RSS file behaviours

--- src/rss/rules/file.rb
+++ src/rss/rules/file.rb
@@ -5,13 +5,17 @@ module RSS
   module Rules
     module File
       class << self
-        def load_file
-          FileUtils.cp 'rss.example.json', 'rss.json' unless ::File.exist?('rss.json')
-          JSON.parse(::File.read('rss.json'))
+        def load_file(file_path = 'rss.json')
+          if file_path == 'rss.json'
+            FileUtils.cp 'rss.example.json', 'rss.json' unless ::File.exist?('rss.json')
+          end
+          @file_path = file_path
+          JSON.parse(::File.read(file_path))
         end
 
+
         def save(hash)
-          ::File.write('rss.json', JSON.pretty_generate(hash))
+          ::File.write(@file_path, JSON.pretty_generate(hash))
         end
       end
     end
